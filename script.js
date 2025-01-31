0843c54bbf0bbfd943fe4cb920ef4b7262ed16b4
RRenann <renancarlosmoreira71@gmail.com>
2025-01-30 21:41:03 -0300

    Commit projeto spotify

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*SCRIPT JS*/
+
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
